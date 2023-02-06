70a92b50eb3c495537bc5d709a1131397a59fd1e
allisonnault <allisonrnault@gmail.com>
2023-02-06 15:18:35 -0600

    added 2nd api fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
-var dogBreedURL = "https://api.thedogapi.com/v1/breeds?limit=10&page=0"
+// var dogBreedURL = "https://api.thedogapi.com/v1/breeds?limit=10&page=0"
 
-fetch(dogBreedURL)
+// fetch(dogBreedURL)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+// console.log(data);
+//         });
+
+
+var nijaDogKey = "mdyq9NKoVyj8PyZs+WRkkQ==IML8Igs43m66eDga"
+var breed = "yorkshire terrier"
+var nijaDogURL = "https://api.api-ninjas.com/v1/dogs?name="+breed;
+
+fetch(nijaDogURL, {headers:{'X-Api-Key': 'mdyq9NKoVyj8PyZs+WRkkQ==IML8Igs43m66eDga'}})
         .then(function (response) {
             return response.json();
         })
