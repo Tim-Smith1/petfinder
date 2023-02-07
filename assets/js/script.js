49cb9d954aae0439be4ab578d9c267374dc2dbdc
allisonnault <allisonrnault@gmail.com>
2023-02-07 11:21:47 -0600

    added breed facts

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,16 +12,42 @@ fetch(nijaDogURL, {headers:{'X-Api-Key': 'mdyq9NKoVyj8PyZs+WRkkQ==IML8Igs43m66eD
 console.log(data);
         });
 
-// Pet finder
-// var petFinderURL = 'https://api.petfinder.com/v2/animals';
-// var petFinderKey = "vlhqQw3I1th5yoCvFcQJDga3QwH9nYp3faRaS2SK3Ckw8vuHsi";
-// var access_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJ2bGhxUXczSTF0aDV5b0N2RmNRSkRnYTNRd0g5bllwM2ZhUmFTMlNLM0Nrdzh2dUhzaSIsImp0aSI6Ijk1ZjI0ZjYwMjcxNTk3MDg5MWVhNmM4NDAxZWNiZGI3NDkyNTdjZDRkMDdkNTViYWUwN2I2OWUxMTQxZGVkOTU5MTExNTE2ZTQ1ODAyNGE3IiwiaWF0IjoxNjc1NzMyNjM2LCJuYmYiOjE2NzU3MzI2MzYsImV4cCI6MTY3NTczNjIzNiwic3ViIjoiIiwic2NvcGVzIjpbXX0.uz1dAhTj-7hWjsqoIP3hMzksIVJTqoLL53KbASfm_XnoRgD3YejpuDygOD279pvlbGXbwNr2vN5jVTcMbbOTajOfROIs1cjRTF-XNSv_8u3nZdelQerdueHNFLv6OFUpQqL56mZtmTqRT7TuTAQmgPCLXjLu7METJXFoVuHxSVGymZJY9SV7rbTXnzHcvAJh44cahmE6Y_TY2LZjzqL4HRLMyCma4XUChR2qd1tUb6KrMkO9QWJNIXD8CEvIBwATRm1VDkQAb-Sb73EccAHhnki-JL7Xiedi13VdDp6ad-gqcvJr1Y5IYxJxFjBu1R1zojD6n7epk1yLoWhobkjj7A';
-// var postalCode = "60622";
-// fetch(petFinderURL + '?location='+ postalCode, {headers:{'Authorization': 'Bearer ' + access_token}})
-// .then(function (response) {
-//     return response.json();
-// })
-// .then(function (data) {
+// relevant data:
+// breedFacts = {
+//         barking: data[0].barking,
+//         drooling: data[0].drooling,
+//         energy: data[0].energy,
+//         goodWithChildren: data[0].good_with_children,
+//         goodWithDogs: data[0].good_with_other_dogs,
+//         maxHeight: data[0].max_height_male,
+//         minHeight: data[0].min_height_female,
+//         maxWeight: data[0].max_weight_male,
+//         minWeight: data[0].min_weight_female,
+//         minLifeEx: data[0].min_life_expectancy,
+//         playfulness: data[0].playfulness,
+//         shedding: data[0].dhedding,
+//         trainability: data[0].trainability 
+// }
 
-//     console.log(data);
-// })
+
+
+
+
+// Pet finder
+var petFinderURL = 'https://api.petfinder.com/v2/animals';
+var petFinderKey = "vlhqQw3I1th5yoCvFcQJDga3QwH9nYp3faRaS2SK3Ckw8vuHsi";
+var access_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJ2bGhxUXczSTF0aDV5b0N2RmNRSkRnYTNRd0g5bllwM2ZhUmFTMlNLM0Nrdzh2dUhzaSIsImp0aSI6IjZiMGM4NDhhZGNkZWQyODAyMjFmZGJiNDU0YTM1NTJhMzYzZTI0ZWFhY2Y4NjhmNmY2MDhhNTdkYjkxOGJmNjhmODg3NmJkYjk4ZmEwNzNiIiwiaWF0IjoxNjc1Nzg5NjkyLCJuYmYiOjE2NzU3ODk2OTIsImV4cCI6MTY3NTc5MzI5MSwic3ViIjoiIiwic2NvcGVzIjpbXX0.Yxi0UgDzrXX7ST1MjMHhNE_AqPKH8QelWDvhNHVHXh7CnBUZLeJlTVRgeKReP0GSDSmJ-i9UExY1gneY8YDGWIwj3T8KEVq3GO7Te6kbd2sb4YFc253lCaBr0DYVHrnYwK-PNxt7Ue5VwR7AzjU3E5SSJT3buS-7RJJXX8y34X2fqi2pFAjdiJybSkFvcnMqDiaiJjPS5xsSqvyPN3_VIDy8Rmk2jG2P7LmuQWASC2BfGihlW_aIj0O2EJ0M7ck399vujY6HyAcrtag0nHQic61zAb-9SAH2HyuDr0hxg79JY_J0BTLlM9zLIdkuM1WR5IieXC74JM2NgQBox8T4yA';
+var postalCode = "60622";
+fetch(petFinderURL + '?location='+ postalCode, {headers:{'Authorization': 'Bearer ' + access_token}})
+.then(function (response) {
+    return response.json();
+})
+.then(function (data) {
+
+    console.log(data);
+})
+
+// relavant data:
+// petFinderInfo = {
+
+// }
