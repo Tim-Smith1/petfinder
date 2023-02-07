2c99ec352f7835e42ca03a26932195ee152832e2
allisonnault <allisonrnault@gmail.com>
2023-02-07 10:49:08 -0600

    added a 2nd fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,5 @@
-// var dogBreedURL = "https://api.thedogapi.com/v1/breeds?limit=10&page=0"
 
-// fetch(dogBreedURL)
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         .then(function (data) {
-// console.log(data);
-//         });
-
-
+//  nija dogs
 var nijaDogKey = "mdyq9NKoVyj8PyZs+WRkkQ==IML8Igs43m66eDga"
 var breed = "yorkshire terrier"
 var nijaDogURL = "https://api.api-ninjas.com/v1/dogs?name="+breed;
@@ -20,3 +11,17 @@ fetch(nijaDogURL, {headers:{'X-Api-Key': 'mdyq9NKoVyj8PyZs+WRkkQ==IML8Igs43m66eD
         .then(function (data) {
 console.log(data);
         });
+
+// Pet finder
+// var petFinderURL = 'https://api.petfinder.com/v2/animals';
+// var petFinderKey = "vlhqQw3I1th5yoCvFcQJDga3QwH9nYp3faRaS2SK3Ckw8vuHsi";
+// var access_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJ2bGhxUXczSTF0aDV5b0N2RmNRSkRnYTNRd0g5bllwM2ZhUmFTMlNLM0Nrdzh2dUhzaSIsImp0aSI6Ijk1ZjI0ZjYwMjcxNTk3MDg5MWVhNmM4NDAxZWNiZGI3NDkyNTdjZDRkMDdkNTViYWUwN2I2OWUxMTQxZGVkOTU5MTExNTE2ZTQ1ODAyNGE3IiwiaWF0IjoxNjc1NzMyNjM2LCJuYmYiOjE2NzU3MzI2MzYsImV4cCI6MTY3NTczNjIzNiwic3ViIjoiIiwic2NvcGVzIjpbXX0.uz1dAhTj-7hWjsqoIP3hMzksIVJTqoLL53KbASfm_XnoRgD3YejpuDygOD279pvlbGXbwNr2vN5jVTcMbbOTajOfROIs1cjRTF-XNSv_8u3nZdelQerdueHNFLv6OFUpQqL56mZtmTqRT7TuTAQmgPCLXjLu7METJXFoVuHxSVGymZJY9SV7rbTXnzHcvAJh44cahmE6Y_TY2LZjzqL4HRLMyCma4XUChR2qd1tUb6KrMkO9QWJNIXD8CEvIBwATRm1VDkQAb-Sb73EccAHhnki-JL7Xiedi13VdDp6ad-gqcvJr1Y5IYxJxFjBu1R1zojD6n7epk1yLoWhobkjj7A';
+// var postalCode = "60622";
+// fetch(petFinderURL + '?location='+ postalCode, {headers:{'Authorization': 'Bearer ' + access_token}})
+// .then(function (response) {
+//     return response.json();
+// })
+// .then(function (data) {
+
+//     console.log(data);
+// })
