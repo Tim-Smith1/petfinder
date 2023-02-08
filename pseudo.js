7ef7f0dbd832db2b57665369d9e21f1336a059fd
allisonnault <allisonrnault@gmail.com>
2023-02-08 13:55:58 -0600

    saving before merging with main

--- pseudo.js
+++ pseudo.js
@@ -56,4 +56,4 @@
 // }
 
 
-curl -d "grant_type=client_credentials&client_id=vlhqQw3I1th5yoCvFcQJDga3QwH9nYp3faRaS2SK3Ckw8vuHsi&client_secret=0lu8umPP2fDm04fyPgehlUvX8qObWDU2wT5jMUQH" https://api.petfinder.com/v2/oauth2/token
+// curl -d "grant_type=client_credentials&client_id=vlhqQw3I1th5yoCvFcQJDga3QwH9nYp3faRaS2SK3Ckw8vuHsi&client_secret=0lu8umPP2fDm04fyPgehlUvX8qObWDU2wT5jMUQH" https://api.petfinder.com/v2/oauth2/token
