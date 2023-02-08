90bf59159762060bd2d4f54501c9806074855c88
allisonnault <allisonrnault@gmail.com>
2023-02-08 11:12:06 -0600

    search form imput works

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,43 +45,47 @@
 //         trainability: data[0].trainability 
 // }
 
-// var searchBtn = $('#searchBtn');
+var searchBtn = $('#searchBtn');
 
 
 
-// function searchFormSubmit(event) {
-//         event.preventDefault();
-//         var zipCode = $('.input').val;
-//         var dogAge = $('#dog-age').val;
-//         var dogSize = $('#dog-size').val;
-//         var gender = $('#gender').val;
-//         console.log(zipCode);
-//         console.log(dogAge);
-//         console.log(dogSize);
-//         console.log(gender);
-// }
+function searchFormSubmit(event) {
+        event.preventDefault();
+        var zipCode = $('.input').val();
+        var dogAge = $('#dog-age').val();
+        var dogSize = $('#dog-size').val();
+        var gender = $('#gender').val();
 
-// searchBtn.on('submit', searchFormSubmit)
+        fetch(petFinderURL + '&size=small' + dogSize + '&age=' + dogAge + '&gender=' + gender + '&location=' + zipCode, { headers: { 'Authorization': 'Bearer ' + access_token } })
+                .then(function (response) {
+                        return response.json();
+                })
+                .then(function (data) {
+
+                        console.log(data);
+                })
+
+}
+
+searchBtn.on('click', searchFormSubmit)
 
 
 // Pet finder
-var petFinderURL = 'https://api.petfinder.com/v2/animals?type=dog&size=small';
+var petFinderURL = 'https://api.petfinder.com/v2/animals?type=dog';
 var petFinderKey = "vlhqQw3I1th5yoCvFcQJDga3QwH9nYp3faRaS2SK3Ckw8vuHsi";
 var access_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJ2bGhxUXczSTF0aDV5b0N2RmNRSkRnYTNRd0g5bllwM2ZhUmFTMlNLM0Nrdzh2dUhzaSIsImp0aSI6IjQ4NDg0YjI3ZjNhMzcwMGUwM2ExMjdhMWVkZGYzZTBhN2JlN2E1NmZiZjdjNmVjMGJlNjg4ZGE5N2JiMzE4MDU1YTYxOTcyZjgxYTgyZTRlIiwiaWF0IjoxNjc1ODcyNDM1LCJuYmYiOjE2NzU4NzI0MzUsImV4cCI6MTY3NTg3NjAzNSwic3ViIjoiIiwic2NvcGVzIjpbXX0.C0HWrwMYf8DWtgv5Z4F95FFMdLJQtxmNSHDnrIxTpgPtjVYLtc37eTHHi7qnMwctbasROG-pmPrNE3-LyGZSQ4yTHAhwYZ6K5GGTPCltt9fMH5oQ19xmOMX7XLh2vsNmei8siolqyelQ8MbdeIMtUQPSCY3vmjmBnGTZU2l9_H-PZJwS72TcxiA0agNVGu_7ljq3bZjV8mk8jnzh1kcCcM4pLnUm_Osn8QOAMvXwHazZ3Fgtnpr-52vPcDWHC8bmQCUTF_1geeBl6WRiJUFCaVty4z4nYmsuN-x8WeVF6nQ0_O6PrhMMOsQz7M0DWpUe5eCu5GJmlsTSj8uGVXJUWg';
 
-// + dogSize + '&age=' + dogAge + '&gender=' + gender + '&location=' + postalCode
 
-fetch(petFinderURL, {headers:{'Authorization': 'Bearer ' + access_token}})
-.then(function (response) {
-    return response.json();
-})
-.then(function (data) {
 
-    console.log(data);
-})
+
 
 // relavant data:
 // petFinderInfo = {
 
+// }
+
+// var dogFacts = {
+// age: data[i].age,
+// breeds:  
 // }
 
