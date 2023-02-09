76af9a05c42e19e74a3f3e203ddd6e90abf2db76
Tim Smith <meckaleck@hotmail.com>
2023-02-08 19:59:40 -0600

    HTML p3 dog attr and stock photo

--- assets/js/script3.js
+++ assets/js/script3.js
@@ -4,22 +4,44 @@
 
 // var breed = 'pitbull';
 
-var nijaDogKey = "oeZRCFASXWqpKLVdC4S1qg==0vv7FTjSoPeONrA6"
-var breed = "10"
-var nijaDogURL = "https://api.api-ninjas.com/v1/dogs?offset="+breed;
+var ninjaDogKey = "oeZRCFASXWqpKLVdC4S1qg==0vv7FTjSoPeONrA6"
+var breed = "golden";
+var energy = "3";
+var ninjaDogURL = "https://api.api-ninjas.com/v1/dogs?name=" + breed;// + '&energy=' + energy;
 
-fetch(nijaDogURL, {headers:{'X-Api-Key': 'oeZRCFASXWqpKLVdC4S1qg==0vv7FTjSoPeONrA6'}})
+//var barking = document.getElementById('barking').innerHTML;
+
+
+
+fetch(ninjaDogURL, {headers:{'X-Api-Key': 'oeZRCFASXWqpKLVdC4S1qg==0vv7FTjSoPeONrA6'}})
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-console.log(data);
+        console.log(data);
+
+        document.getElementById('barking').innerHTML = ('Barking:' + data[0].barking);
+        document.getElementById('drooling').innerHTML = ('Drooling:' + data[0].drooling);
+        document.getElementById('energy').innerHTML = ('Energy:' + data[0].energy);
+        document.getElementById('child-friendly').innerHTML = ('Child Friendly:' + data[0].good_with_children);
+        document.getElementById('maxage').innerHTML = ('Max age:' + data[0].max_life_expectancy);
+        document.getElementById('playfulness').innerHTML = ('Playfulness:' + data[0].playfulness);
+        document.getElementById('protective').innerHTML = ('Protective:' + data[0].protectiveness);
+        document.getElementById('shedding').innerHTML = ('Shedding:' + data[0].shedding);
+        document.getElementById('trainability').innerHTML = ('Trainability:' + data[0].trainability);
+        
+
+
+        
         })
 
+        
+
+var dogBreed = "https://api.api-ninjas.com/v1/dogs?name="+breed;
+var dogEnergy = "https://api.api-ninjas.com/v1/dogs?energy="+energy;
 
 
-
-
+//document.getElementById('barking').innerHTML = ('Barking:' +data.list[0].barking);
 
 
 
